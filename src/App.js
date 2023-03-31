1c17a4708b7d13552c1aa4759f62cd347e016f1b
yasmine <yasmineashoush5@gmail.com>
2023-03-31 16:05:15 -0400

    react updates

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Page from "./components/Page";
+import Nav from "./components/Nav";
+
+function App() {
+  const [pages] = useState([
+    {
+      name: "about me"
+    },
+    { name: "portfolio" },
+    { name: "contact" },
+    { name: "resume" }
+
+  ]);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  return (
+    <div>
+      <Header>
+        <Nav>
+          pages={pages}
+          setCurrentPage= {setCurrentPage}
+          currentPage={currentPage}
+        </Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+export default App;
