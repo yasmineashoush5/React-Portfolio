2c3eed2228ef51ee80ed96f2344ad950c7a2132f
yasmine <yasmineashoush5@gmail.com>
2023-07-31 10:59:14 -0400

    changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,7 @@ function About() {
                     My name is Yasmine Mohamed. Currently, I work in the school system, and through my teaching career, I learned to be patient, team player, creative, passionate, multi-tasking, and working under pressure .I read a lot about coding and my dream is to be a full stack web developer.
                 </p>
                 <p>
-                    I decided to study coding and I knew it is going to be a long and challenging journey.But I also knew that by hard work and dedication I would reach to the top of the mountain and finally fulfill all my dreams. And here we go, I am about to graduate and start my new career.
+                    I decided to study coding and I knew it is going to be a long and challenging journey.But I also knew that by hard work and dedication I would reach to the top of the mountain and finally fulfill all my dreams. And Finally, I graduated and I can't wait to start my new career, and take my very first steps towards my amazing journey.
                 </p>
             </div>
         </section>
